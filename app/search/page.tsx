6b476862ee6c3264521f2144e384e5874d6b1b2d
Leon Carbonell <leoncarbonell@gmail.com>
2024-01-11 16:53:23 +1100

    Google Places API endpoint

---
+++ app/search/page.tsx
@@ -0,0 +1,11 @@
+import SearchPageComponent from "@/components/pages/search/searchpage";
+
+function SearchPage() {
+  return (
+    <>
+      <SearchPageComponent />
+    </>
+  );
+}
+
+export default SearchPage;
---
+++ components/pages/search/searchpage.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+function SearchPageComponent() {
+  return <>Component Goes Here</>;
+}
+
+export default SearchPageComponent;
