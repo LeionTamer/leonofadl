21d9b88bbf01083625f45c588463213674004dca
Leon Carbonell <leoncarbonell@gmail.com>
2024-01-14 16:38:05 +1100

    Added options

--- components/google/autocomplete.tsx
+++ components/google/autocomplete.tsx
@@ -1,12 +1,18 @@
 'use client'
 
 import { useDebouncedState } from '@mantine/hooks'
-import { Command, CommandInput } from '../ui/command'
-import { FC, useEffect } from 'react'
+import {
+  Command,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from '../ui/command'
+import { FC, useEffect, useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import {
   PlaceAutocompleteResponse,
-  PlaceAutocompleteResponseData,
+  PlaceAutocompleteResult,
 } from '@googlemaps/google-maps-services-js'
 import axios from 'axios'
 
@@ -22,6 +28,7 @@ const GoogleAutocomplete: FC<IGoogleAutocompleteProps> = ({
   ...props
 }) => {
   const [search, setSearch] = useDebouncedState('', 500)
+  const [options, setOptions] = useState<PlaceAutocompleteResult[]>([])
 
   const searchFn = async (): Promise<PlaceAutocompleteResponse | undefined> => {
     if (search.length >= 3) {
@@ -37,23 +44,39 @@ const GoogleAutocomplete: FC<IGoogleAutocompleteProps> = ({
     enabled: search.length >= 3,
   })
 
+  const PlacesList = () => {
+    if (options.length <= 0) return null
+
+    return (
+      <CommandList>
+        <CommandGroup heading="List of places">
+          {options.map((entry) => (
+            <CommandItem key={entry.place_id}>{entry.description}</CommandItem>
+          ))}
+        </CommandGroup>
+      </CommandList>
+    )
+  }
+
   useEffect(() => {
-    // if (search.length <= 2) return
+    if (search.length <= 2) setOptions([])
     if (search.length >= 3) searchAPI.refetch()
   }, [search])
 
   useEffect(() => {
     if (!!searchAPI.data) {
       console.table(searchAPI.data.data.predictions)
+      setOptions([...searchAPI.data.data.predictions])
     }
   }, [searchAPI.data])
 
   return (
     <Command className="rounded-lg border shadow-md">
       <CommandInput
-        placeholder="Type a command or search..."
+        placeholder="Search for a location"
         onChangeCapture={(event) => setSearch(event.currentTarget.value)}
       />
+      <PlacesList />
     </Command>
   )
 }
