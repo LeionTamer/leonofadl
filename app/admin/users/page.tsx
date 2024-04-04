d84f60532682f50f56573fd7f661e7c93c9a08f1
Leon Carbonell <leoncarbonell@gmail.com>
2024-04-04 10:10:46 +0800

    feat: Added prisma adapter

---
+++ app/admin/users/page.tsx
@@ -0,0 +1,21 @@
+import { authOptions } from '@/helpers/auth'
+import { prisma } from '@/lib/prisma'
+import { getServerSession } from 'next-auth'
+import { redirect } from 'next/navigation'
+
+async function Users() {
+  const session = await getServerSession(authOptions)
+
+  if (!session) {
+    redirect('/api/auth/signin')
+  } else {
+    if (session.user.email !== 'leoncarbonell@gmail.com') {
+      redirect('/')
+    }
+  }
+
+  const users = await prisma.user.findMany()
+  return <>{users.map((user) => user.name)}</>
+}
+
+export default Users
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,19 @@
 import NextAuth, { AuthOptions } from "next-auth"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import GoogleProvider from "next-auth/providers/google"
+import { prisma } from "@/lib/prisma"
+import { authOptions } from "@/helpers/auth"
 
-const authOptions: AuthOptions = {
-  secret: process.env.NEXTAUTH_SECRET,
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    })
-  ]
-}
+// export const authOptions: AuthOptions = {
+//   adapter: PrismaAdapter(prisma),
+//   secret: process.env.NEXTAUTH_SECRET,
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID!,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+//     })
+//   ]
+// }
 
 const handler = NextAuth(authOptions)
 export { handler as GET, handler as POST }
