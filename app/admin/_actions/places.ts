1a5135b4a91cb98853aa00917a039736125dda0c
Leon Carbonell <leoncarbonell@gmail.com>
2024-04-27 15:50:47 +0930

    ffeat: added form inputs

--- app/admin/_actions/places.ts
+++ app/admin/_actions/places.ts
@@ -3,14 +3,15 @@
 import { placeFormSchema } from '../_types/place'
 
 export async function addPlace(prevState: unknown, formData: FormData) {
-  console.table(Object.fromEntries(formData.entries()))
+  console.table(formData)
   const result = placeFormSchema.safeParse(
     Object.fromEntries(formData.entries())
   )
 
+  console.table(result)
+
   if (result.success === false) {
+    console.table(result.error.formErrors.fieldErrors)
     return result.error.formErrors.fieldErrors
   }
-
-  const data = result.data
 }
--- app/admin/places/_components/PlaceForm.tsx
+++ app/admin/places/_components/PlaceForm.tsx
@@ -7,6 +7,7 @@ import {
   FormField,
   FormItem,
   FormLabel,
+  FormMessage,
 } from '@/components/ui/form'
 import { useForm } from 'react-hook-form'
 import { Input } from '@/components/ui/input'
@@ -15,23 +16,139 @@ import { useEffect } from 'react'
 import { Button } from '@/components/ui/button'
 import { addPlace } from '../../_actions/places'
 import { useFormState } from 'react-dom'
-import { PlaceFormType } from '../../_types/place'
+import { PlaceFormType, placeFormSchema } from '../../_types/place'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 const PlaceForm = ({ place }: { place?: PlaceFormType }) => {
   const { state: deckState } = useDeckStateContext()
-  const [state, action] = useFormState(addPlace, null)
+  const [actionState, action] = useFormState(addPlace, null)
+
+  const form = useForm<PlaceFormType>({
+    resolver: zodResolver(placeFormSchema),
+    defaultValues: {
+      placeId: '',
+      name: '',
+      address: '',
+      googleUrl: '',
+      latitude: '' as unknown as number,
+      longtitude: '' as unknown as number,
+    },
+  })
 
   useEffect(() => {
-    if (deckState.googlePlaceDetails) {
-      console.table(deckState.googlePlaceDetails)
+    if (
+      !!deckState.googlePlaceDetails &&
+      !!deckState.googlePlaceDetails.place_id
+    ) {
+      const details = deckState.googlePlaceDetails
+      form.setValue('name', details.name!)
+      form.setValue('address', details.formatted_address!)
+      form.setValue('latitude', details.geometry!.location.lat)
+      form.setValue('longtitude', details.geometry!.location.lng)
+      form.setValue('placeId', details.place_id!)
+      form.setValue('googleUrl', details.url || '')
     }
   }, [deckState.googlePlaceDetails])
 
   return (
     <div className="mt-10">
-      <form action={action}>
-        <Button type="submit">Submit</Button>
-      </form>
+      <Form {...form}>
+        <form action={action}>
+          <FormField
+            control={form.control}
+            name="placeId"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Google Place ID</FormLabel>
+                <FormControl>
+                  <Input placeholder="Google Place ID" {...field} disabled />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="name" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="address"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Address</FormLabel>
+                <FormControl>
+                  <Input placeholder="address" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex flex-row gap-10">
+            <FormField
+              control={form.control}
+              name="latitude"
+              render={({ field }) => (
+                <FormItem className="w-[50%]">
+                  <FormLabel>Lat</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="latitude"
+                      {...field}
+                      type="number"
+                      disabled
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="longtitude"
+              render={({ field }) => (
+                <FormItem className="w-[50%]">
+                  <FormLabel>Lon</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="longtitude"
+                      {...field}
+                      type="number"
+                      disabled
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormField
+            control={form.control}
+            name="googleUrl"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Google URL</FormLabel>
+                <FormControl>
+                  <Input placeholder="Google URL" {...field} type="url" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="mt-10 w-full">
+            Submit
+          </Button>
+        </form>
+      </Form>
     </div>
   )
 }
