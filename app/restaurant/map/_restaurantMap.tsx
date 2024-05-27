8aa97ec7e92d9e236aaa755a7e4eb81d03dfe27f
Leon Carbonell <leoncarbonell@gmail.com>
2024-05-27 21:42:35 +0930

    feat: Add click functionality to restaurant map markers

--- app/restaurant/map/_restaurantMap.tsx
+++ app/restaurant/map/_restaurantMap.tsx
@@ -7,20 +7,20 @@ import {
 import BaseMap from '@/components/deckgl/basemap'
 import { Restaurant } from '@prisma/client'
 import { GeoJsonLayer } from 'deck.gl/typed'
-// import { multiPoint } from '@turf/helpers'
-import { FeatureCollection } from 'geojson'
-import { useEffect } from 'react'
+import { FeatureCollection, Feature, Geometry } from 'geojson'
+import { useEffect, useState } from 'react'
 
 interface IRestarurantMapViewProps {
   restaurants: Restaurant[]
 }
 
 function RestaurantMapView({ restaurants }: IRestarurantMapViewProps) {
+  const [] = useState(10)
   const geoData: FeatureCollection = {
     type: 'FeatureCollection',
     features: restaurants.map((restaurant) => ({
       type: 'Feature',
-      properties: { name: restaurant.name },
+      properties: { name: restaurant.name, id: restaurant.id },
       geometry: {
         coordinates: [restaurant.longtitude, restaurant.latitude],
         type: 'Point',
@@ -28,7 +28,10 @@ function RestaurantMapView({ restaurants }: IRestarurantMapViewProps) {
     })),
   }
 
-  const { state } = useDeckStateContext()
+  const { state, dispatch } = useDeckStateContext()
+
+  // const zoom = state.viewState.zoom
+  // const radius = zoom <= 10 ? 10 : zoom <= 15 ? 20 : 30
 
   const geoLayer = new GeoJsonLayer({
     id: 'geojson-layer',
@@ -38,21 +41,22 @@ function RestaurantMapView({ restaurants }: IRestarurantMapViewProps) {
     filled: true,
     extruded: true,
     getFillColor: [250, 0, 0, 255],
-    getPointRadius: 20,
+    getPointRadius: 10,
     getLineWidth: 10,
     getElevation: 30,
+    onClick: (info) => {
+      const properties = info.object.properties
+      const [longitude, latitude] = info.object.geometry.coordinates as [
+        number,
+        number,
+      ]
+      const viewState = state.viewState
+      dispatch({
+        viewState: { ...viewState, longitude, latitude, zoom: 16 },
+      })
+    },
   })
 
-  useEffect(() => {
-    // const geoData = multiPoint(
-    //   restaurants.map((restaurants) => [
-    //     restaurants.longtitude,
-    //     restaurants.latitude,
-    //   ])
-    // )
-    // console.table(geoData)
-    // dispatch({ featurePoints: geoData })
-  }, [restaurants])
   return <BaseMap height="100vh" layers={[geoLayer]} />
 }
 
