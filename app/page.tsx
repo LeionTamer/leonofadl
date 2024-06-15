dbc85613e92c3fdfba0a6d62b14f61e8d37e54c3
Leon Carbonell <leoncarbonell@gmail.com>
2024-06-15 11:58:49 +0930

    fix: style for home

--- app/page.tsx
+++ app/page.tsx
@@ -7,49 +7,31 @@ import { Badge } from '@/components/ui/badge'
 
 export default async function Home() {
   const restaurants = await db.restaurant.findMany({
-    take: 15,
+    take: 5,
   })
 
   return (
     <main className="min-h-screen flex-col items-center justify-between bg-[#ffc971]">
-      <div className="mx-auto flex h-[300px] max-w-7xl flex-col items-center justify-center text-center text-5xl md:text-7xl">
+      <div className="mx-auto flex h-72 max-w-7xl flex-col items-center justify-center text-center text-4xl md:text-5xl">
         {"Elevate Your Eating Experience with Leon's Digital Twin!"}
       </div>
       <div className="mx-auto flex max-w-7xl flex-wrap justify-start">
-        <div className="flex h-[600px] basis-1 flex-col items-center justify-center bg-red-200 px-10 text-3xl md:basis-1/2 md:text-4xl">
-          Unleash your inner foodie with the ultimate guide to the best
-          restaurants in Adelaide. Your perfect dining experience awaits!
+        <div className="h-90 w-full content-center bg-red-200 p-10 align-middle md:w-1/2 md:px-10">
+          <div className="flex-col content-center items-center justify-center text-2xl md:basis-1/2 md:text-3xl">
+            Unleash your inner foodie with the ultimate guide to the best
+            restaurants in Adelaide. Your perfect dining experience awaits!
+          </div>
         </div>
-        <div className="flex h-[600px] basis-1 flex-col items-center justify-center px-10 md:basis-1/2">
-          <Card className="min-h-full w-full overflow-y-auto">
-            <CardHeader>
-              <CardTitle>Latest Updates</CardTitle>
-            </CardHeader>
-            <CardContent>
-              {/* <div className="flex gap-2"> */}
-              {restaurants.map((restaurant, index) => (
-                <div key={restaurant.id} className="my-5">
-                  {index >= 1 && <Separator />}
-                  <div className="my-2 text-xl">{restaurant.name}</div>
-                  {!!restaurant.website && (
-                    <div className="w-full py-2 text-blue-700">
-                      <Link href={restaurant.website} target="_blank">
-                        {restaurant.website}
-                      </Link>
-                    </div>
-                  )}
-                  {!!restaurant.tags && (
-                    <div className="flex gap-2">
-                      {restaurant.tags.map((tag, index) => (
-                        <Badge key={index}>{tag}</Badge>
-                      ))}
-                    </div>
-                  )}
-                </div>
-              ))}
-              {/* </div> */}
-            </CardContent>
-          </Card>
+        <div className="h-90 mx-auto w-full flex-row overflow-y-auto p-5 md:w-1/2">
+          <div className="mb-5 text-2xl">Latest</div>
+          <div className="my-auto">
+            {restaurants.map((restaurant, index) => (
+              <div key={restaurant.id} className="my-2">
+                {index >= 1 && <Separator />}
+                <div className="my-2 truncate text-xl">{restaurant.name}</div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </main>
