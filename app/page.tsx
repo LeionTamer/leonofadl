1721359471d86923045f1197693801bb60414bc9
Leon Carbonell <leoncarbonell@gmail.com>
2024-06-16 14:06:48 +0930

    feat: add links to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -16,21 +16,39 @@ export default async function Home() {
         {"Elevate Your Eating Experience with Leon's Digital Twin!"}
       </div>
       <div className="mx-auto flex max-w-7xl flex-wrap justify-start">
-        <div className="h-90 w-full content-center bg-red-200 p-10 align-middle md:w-1/2 md:px-10">
-          <div className="flex-col content-center items-center justify-center text-2xl md:basis-1/2 md:text-3xl">
+        <div className="h-90 flex w-full flex-col justify-between bg-red-200 p-5 align-middle md:w-1/2">
+          <div className="flex-col content-center items-center justify-center p-5 text-2xl md:basis-1/2 md:text-3xl">
             Unleash your inner foodie with the ultimate guide to the best
             restaurants in Adelaide. Your perfect dining experience awaits!
           </div>
+          <div className="my-8 w-full text-right text-2xl text-orange-700 md:text-3xl">
+            <Link href="/explore">
+              Explore{' '}
+              <span className="material-symbols-outlined align-middle">
+                arrow_forward
+              </span>
+            </Link>
+          </div>
         </div>
         <div className="h-90 mx-auto w-full flex-row overflow-y-auto p-5 md:w-1/2">
-          <div className="mb-5 text-2xl">Latest</div>
-          <div className="my-auto">
-            {restaurants.map((restaurant, index) => (
-              <div key={restaurant.id} className="my-2">
-                {index >= 1 && <Separator />}
-                <div className="my-2 truncate text-xl">{restaurant.name}</div>
-              </div>
-            ))}
+          <div>
+            <div className="mb-5 text-2xl">Latest</div>
+            <div className="my-auto">
+              {restaurants.map((restaurant, index) => (
+                <div key={restaurant.id} className="my-2">
+                  {index >= 1 && <Separator />}
+                  <div className="my-2 truncate text-xl">{restaurant.name}</div>
+                </div>
+              ))}
+            </div>
+            <div className="my-8 w-full text-right text-2xl text-orange-700 md:text-3xl">
+              <Link href="/restaurant">
+                View Restaurants{' '}
+                <span className="material-symbols-outlined align-middle">
+                  arrow_forward
+                </span>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
